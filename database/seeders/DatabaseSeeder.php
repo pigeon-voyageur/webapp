a668b45cb04fd412fcf22136aa65a8e33806b9f3
Arthaud <arthaudp33@gmail.com>
2023-10-18 17:58:41 +0200

    feat: add test user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,7 +5,6 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\User;
 use Illuminate\Database\Seeder;
-use Database\Seeders\ParchmentSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,6 +14,10 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         User::factory(10)->create();
+        User::factory()->create([
+            'name' => 'Testman',
+            'email' => 'test@example.com',
+        ]);
 
         $this->call([
             ParchmentSeeder::class,
