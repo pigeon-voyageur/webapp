fa323e924b1abb6be9bb8042c179a30816222478
Francia-bdz <francia.bidzimou@mmibordeaux.com>
2023-10-23 15:50:33 +0200

    feat: function isTravelling

--- app/Models/Pigeon.php
+++ app/Models/Pigeon.php
@@ -2,28 +2,31 @@
 
 namespace App\Models;
 
-use App\Models\User;
-use App\Models\PigeonMessage;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Pigeon extends Model
 {
     use HasFactory;
-    
+
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
-  
+
     public function news(): BelongsToMany
     {
         return $this->belongsToMany(News::class, 'pigeon_messages')->using(PigeonMessage::class);
-    }    
+    }
 
-        /**
+    public function isTravelling(): bool
+    {
+        return $this->news()->wherePivot('arrival_date', '>=', now())->count();
+    }
+
+    /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
