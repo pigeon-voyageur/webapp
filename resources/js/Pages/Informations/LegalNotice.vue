746538cf47809223579a73b0bc8944d2e58d85b9
Arthaud <arthaudp33@gmail.com>
2023-10-26 10:51:09 +0200

    content: add legal notice

---
+++ resources/js/Pages/Informations/LegalNotice.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import H1 from '@/Components/Primitives/H1.vue';
+import InformationsLayout from "@/Layouts/InformationsLayout.vue";
+import H2 from "@/Components/Primitives/H2.vue";</script>
+<template>
+    <InformationsLayout>
+        <H1>Mentions légales</H1>
+
+        <div>
+            <H2>Note d'information</H2>
+            <p>
+                Ce site et l'entièreté du contenu en rapport ont été réalisé dans le cadre d'un projet du BUT MMI à l'IUT Montaigne.
+            </p>
+        </div>
+
+        <div>
+            <H2>Identité et contact</H2>
+            <p>
+                Le site est édité par Pigeon, Association (fictive) loi 1901.
+            </p>
+        </div>
+
+        <div>
+            <H2>Contact</H2>
+            <p>
+                Vous pouvez nous contacter à l'adresse contact@pigeon.arthaud.ovh.
+            </p>
+            <p>
+                Le siège de l'association est domicilié au 1 Rue Jacques Ellul, 33800 Bordeaux.
+            </p>
+        </div>
+
+        <div>
+            <H2>Hébergement</H2>
+            <p>
+                Le site est hébergé chez o2switch.com
+            </p>
+            <ul class="list-disc ml-4 pb-4">
+                <li>Adresse: Chemin des Pardiaux 63000 Clermont-Ferrand</li>
+                <li>Tel: 04 44 44 60 40</li>
+                <li>E-mail: support@o2switch.fr</li>
+            </ul>
+        </div>
+    </InformationsLayout>
+</template>
